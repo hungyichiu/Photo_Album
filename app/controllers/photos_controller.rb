9c41cc25d9ddf7ae26a82dbd97e068829e2a8c85
Chris Chiu <hungyichiu@gmail.com>
2017-11-13 04:48:09 +0800

    add pg, update Gemfile, change production db to pg

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,58 @@
+class PhotosController < ApplicationController
+  before_action :set_photo, :only => [:show, :edit, :update, :destroy]
+
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to photos_url
+
+      #redirect_to :action => :index
+      #重新request 刷新頁面
+    else
+      render :action => :new
+      #render 類似JS的部份更新
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    
+    if @photo.update_attributes(photo_params)
+      redirect_to photo_path(@photo)
+    else
+      render :action => :edit
+    end
+    
+  end
+
+def destroy
+  
+  @photo.destroy
+
+  redirect_to photos_url
+  
+end
+
+  private
+
+  def set_photo
+    @photo = Photo.find(params[:id])
+  end
+
+  def photo_params
+    params.require(:photo).permit(:title, :date, :description, :file_location)
+  end
+end
